34370b48399840228b603332e9c1b61a71bd00df
Paul J <jorpaulvincent@gmail.com>
2022-05-01 11:31:06 +0800

    set to headless browser

--- Test/test_1.py
+++ Test/test_1.py
@@ -5,15 +5,24 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture()
 def get_browser():
     global driver
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    options = Options()
+    options.headless = True
+    driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
     browser = driver
     url = 'http://localhost:8080/'
     browser.get(url)
+
+@pytest.fixture()
+def home_page(get_browser):
+    assert driver.find_element(By.XPATH, "//div[@class='title'][contains(.,'Log In')]").text
+    assert driver.find_element(By.XPATH, "//span[contains(.,'Log In')]")
+    assert driver.find_element(By.XPATH, "//span[contains(.,'Sign Up')]")
 
 @pytest.fixture()
 def login_page(get_browser):
